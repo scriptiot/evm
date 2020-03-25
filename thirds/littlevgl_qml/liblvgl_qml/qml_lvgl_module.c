2c8402ff85d799107a37ba1980c309d2f820ec1b
djf <ding465398889.com>
2020-03-25 22:50:56 +0800

    增加ProgressBar Slider Chart支持

--- thirds/littlevgl_qml/liblvgl_qml/qml_lvgl_module.c
+++ thirds/littlevgl_qml/liblvgl_qml/qml_lvgl_module.c
@@ -18,7 +18,7 @@ static evm_t * global_e;
 
 /********** GC ***********/
 
-static void lv_qml_gc_init(evm_t * e, evm_val_t *old_self, evm_val_t * new_self){
+static void lvgl_qml_gc_init(evm_t * e, evm_val_t *old_self, evm_val_t * new_self){
     lv_obj_t * o = (lv_obj_t*)evm_qml_object_get_pointer(old_self);
     lv_obj_set_user_data(o, (lv_obj_user_data_t)evm_2_intptr(new_self));
     qml_object_gc_init(e, old_self, new_self);
@@ -26,7 +26,7 @@ static void lv_qml_gc_init(evm_t * e, evm_val_t *old_self, evm_val_t * new_self)
 
 /********** Callback ***********/
 
-static void lv_qml_run_callback(evm_t * e, evm_val_t * obj, char * name, int argc, evm_val_t * argv){
+static void lvgl_qml_run_callback(evm_t * e, evm_val_t * obj, char * name, int argc, evm_val_t * argv){
     evm_val_t * m_clicked_fn = evm_prop_get(e, obj, name, 0);
     if( m_clicked_fn != NULL)
         evm_run_callback(e, m_clicked_fn, obj, argv, argc);
@@ -36,22 +36,22 @@ static void lv_qml_Button_event_handler(lv_obj_t * obj, lv_event_t event)
 {
     if(event == LV_EVENT_CLICKED) {
         evm_val_t v = evm_mk_object(lv_obj_get_user_data(obj));
-        lv_qml_run_callback(global_e, &v, "onClicked", 0, NULL);
+        lvgl_qml_run_callback(global_e, &v, "onClicked", 0, NULL);
     }else if(event == LV_EVENT_PRESSED) {
         evm_val_t v = (evm_val_t)lv_obj_get_user_data(obj);
-        lv_qml_run_callback(global_e, &v, "onPressed", 0, NULL);
+        lvgl_qml_run_callback(global_e, &v, "onPressed", 0, NULL);
     }else if(event == LV_EVENT_RELEASED) {
         evm_val_t v = (evm_val_t)lv_obj_get_user_data(obj);
-        lv_qml_run_callback(global_e, &v, "onReleased", 0, NULL);
+        lvgl_qml_run_callback(global_e, &v, "onReleased", 0, NULL);
     }
 }
 
-static void lv_qml_TextInput_event_handler(lv_obj_t * obj, lv_event_t event)
+static void lvgl_qml_TextInput_event_handler(lv_obj_t * obj, lv_event_t event)
 {
     if(event == LV_EVENT_VALUE_CHANGED) {
         evm_val_t v = evm_mk_object(lv_obj_get_user_data(obj));
         evm_qml_write_value(global_e, &v, "text", evm_mk_foreign_string((intptr_t)lv_ta_get_text(obj)));
-        lv_qml_run_callback(global_e, &v, "onTextEdited", 0, NULL);
+        lvgl_qml_run_callback(global_e, &v, "onTextEdited", 0, NULL);
     }
 }
 
@@ -135,6 +135,7 @@ static evm_val_t  qml_Rectangle_color(evm_t * e, evm_val_t *p, int argc, evm_val
         lv_obj_t * obj = (lv_obj_t*)evm_qml_object_get_pointer(p);
         lv_style_t * style = (lv_style_t*)lv_obj_get_style(obj);
         style->body.main_color = lvgl_qml_style_get_color(e, v);
+        style->body.grad_color = style->body.main_color;
         lv_obj_refresh_style(obj);
     }
     return EVM_VAL_UNDEFINED;
@@ -171,11 +172,11 @@ static evm_val_t  qml_Button(evm_t * e, evm_val_t *p, int argc, evm_val_t * v){
     if(argc == 1) parent = evm_qml_object_get_pointer(v);
     obj = (lv_obj_t*)lv_btn_create(parent, NULL);
     if( !obj ) return EVM_VAL_UNDEFINED;
+    lvgl_qml_obj_add_style(obj);
     evm_qml_object_set_pointer(p, obj);
     lv_obj_set_event_cb(obj, lv_qml_Button_event_handler);
-    evm_object_set_init(p, (evm_init_fn)lv_qml_gc_init);
+    evm_object_set_init(p, (evm_init_fn)lvgl_qml_gc_init);
     lv_obj_set_user_data(obj, (lv_obj_user_data_t)evm_2_intptr(p));
-    lvgl_qml_obj_add_style(obj);
     return EVM_VAL_UNDEFINED;
 }
 
@@ -247,10 +248,10 @@ static evm_val_t  qml_CheckBox(evm_t * e, evm_val_t *p, int argc, evm_val_t * v)
     if(argc == 1) parent = evm_qml_object_get_pointer(v);
     obj = (lv_obj_t*)lv_cb_create(parent, NULL);
     if( !obj ) return EVM_VAL_UNDEFINED;
-    evm_qml_object_set_pointer(p, obj);
     lvgl_qml_obj_add_style(obj);
+    evm_qml_object_set_pointer(p, obj);
     lv_obj_set_event_cb(obj, lv_qml_Button_event_handler);
-    evm_object_set_init(p, (evm_init_fn)lv_qml_gc_init);
+    evm_object_set_init(p, (evm_init_fn)lvgl_qml_gc_init);
     lv_obj_set_user_data(obj, (lv_obj_user_data_t)evm_2_intptr(p));
     return EVM_VAL_UNDEFINED;
 }
@@ -282,6 +283,9 @@ static evm_val_t  qml_ScrollView(evm_t * e, evm_val_t *p, int argc, evm_val_t * 
     if(argc == 1) parent = evm_qml_object_get_pointer(v);
     obj = (lv_obj_t*)lv_page_create(parent, NULL);
     if( !obj ) return EVM_VAL_UNDEFINED;
+    lv_page_set_scrl_layout(obj, LV_LAYOUT_PRETTY);
+    lv_page_set_style(obj, LV_PAGE_STYLE_BG, &lv_style_transp_tight);
+    lv_page_set_style(obj, LV_PAGE_STYLE_SCRL, &lv_style_transp_tight);
     evm_qml_object_set_pointer(p, obj);
     lvgl_qml_obj_add_style(obj);
     return EVM_VAL_UNDEFINED;
@@ -298,9 +302,9 @@ static evm_val_t  qml_TextField(evm_t * e, evm_val_t *p, int argc, evm_val_t * v
     if( !obj ) return EVM_VAL_UNDEFINED;
     evm_qml_object_set_pointer(p, obj);
     lv_obj_set_user_data(obj, (lv_obj_user_data_t)evm_2_intptr(p));
-    evm_object_set_init(p, (evm_init_fn)lv_qml_gc_init);
+    evm_object_set_init(p, (evm_init_fn)lvgl_qml_gc_init);
     lv_ta_set_cursor_type(obj, LV_CURSOR_LINE | LV_CURSOR_HIDDEN);
-    lv_obj_set_event_cb(obj, lv_qml_TextInput_event_handler);
+    lv_obj_set_event_cb(obj, lvgl_qml_TextInput_event_handler);
     lvgl_qml_obj_add_style(obj);
     lv_ta_set_one_line(obj, 1);
     return EVM_VAL_UNDEFINED;
@@ -443,6 +447,222 @@ static evm_val_t qml_Window_title(evm_t * e, evm_val_t *p, int argc, evm_val_t *
     return EVM_VAL_UNDEFINED;
 }
 
+/********** ProgressBar ***********/
+
+static evm_val_t  qml_ProgressBar(evm_t * e, evm_val_t *p, int argc, evm_val_t * v){
+    EVM_UNUSED(e);
+    lv_obj_t * obj = NULL;
+    lv_obj_t * parent = lv_scr_act();
+    if(argc == 1) parent = evm_qml_object_get_pointer(v);
+    obj = (lv_obj_t*)lv_bar_create(parent, NULL);
+    if( !obj ) return EVM_VAL_UNDEFINED;
+    evm_qml_object_set_pointer(p, obj);
+    lvgl_qml_obj_add_style(obj);
+    return EVM_VAL_UNDEFINED;
+}
+
+static evm_val_t lvgl_qml_ProgressBar_set_min(evm_t * e, evm_val_t *p, int argc, evm_val_t * v){
+    EVM_UNUSED(e);
+    if( argc >= 1 && evm_is_integer(v)){
+        lv_obj_t * obj = (lv_obj_t*)evm_qml_object_get_pointer( p );
+        lv_bar_set_range(obj, evm_2_integer(v) , lv_bar_get_max_value(obj));
+    }
+    return EVM_VAL_UNDEFINED;
+}
+
+static evm_val_t lvgl_qml_ProgressBar_set_max(evm_t * e, evm_val_t *p, int argc, evm_val_t * v){
+    EVM_UNUSED(e);
+    if( argc >= 1 && evm_is_integer(v)){
+        lv_obj_t * obj = (lv_obj_t*)evm_qml_object_get_pointer( p );
+        lv_bar_set_range(obj, lv_bar_get_min_value(obj),  evm_2_integer(v));
+    }
+    return EVM_VAL_UNDEFINED;
+}
+
+static evm_val_t lvgl_qml_ProgressBar_set_value(evm_t * e, evm_val_t *p, int argc, evm_val_t * v){
+    EVM_UNUSED(e);
+    if( argc >= 1 && evm_is_integer(v)){
+        lv_obj_t * obj = (lv_obj_t*)evm_qml_object_get_pointer( p );
+        lv_bar_set_value(obj, evm_2_integer(v), false);
+    }
+    return EVM_VAL_UNDEFINED;
+}
+
+/********** Slider ***********/
+static void lvgl_qml_Slider_event_handler(lv_obj_t * obj, lv_event_t event)
+{
+    if(event == LV_EVENT_VALUE_CHANGED) {
+        evm_val_t v = (evm_val_t)lv_obj_get_user_data(obj);
+        if (global_e){
+            evm_qml_write_value(global_e, &v, "value", evm_mk_number(lv_slider_get_value(obj)));
+            lvgl_qml_run_callback(global_e, &v, "onValueChanged", 0, NULL);
+        }
+    }
+}
+
+static evm_val_t  qml_Slider(evm_t * e, evm_val_t *p, int argc, evm_val_t * v){
+    EVM_UNUSED(e);
+    lv_obj_t * obj = NULL;
+    lv_obj_t * parent = lv_scr_act();
+    if(argc == 1) parent = evm_qml_object_get_pointer(v);
+    obj = (lv_obj_t*)lv_slider_create(parent, NULL);
+    if( !obj ) return EVM_VAL_UNDEFINED;
+    lvgl_qml_obj_add_style(obj);
+    evm_qml_object_set_pointer(p, obj);
+    lv_obj_set_event_cb(obj, lvgl_qml_Slider_event_handler);
+    evm_object_set_init(p, (evm_init_fn)lvgl_qml_gc_init);
+    lv_obj_set_user_data(obj, (lv_obj_user_data_t)evm_2_intptr(p));
+    return EVM_VAL_UNDEFINED;
+}
+
+static evm_val_t lvgl_qml_Slider_set_value(evm_t * e, evm_val_t *p, int argc, evm_val_t * v){
+    EVM_UNUSED(e);
+    if( argc >= 1 && (evm_is_number(v))){
+        lv_obj_t * obj = (lv_obj_t*)evm_qml_object_get_pointer(p);
+        lv_slider_set_value(obj, evm_2_integer(v), LV_ANIM_OFF);
+    }
+    return EVM_VAL_UNDEFINED;
+}
+
+static evm_val_t lvgl_qml_Slider_set_min(evm_t * e, evm_val_t *p, int argc, evm_val_t * v){
+    EVM_UNUSED(e);
+    if( argc >= 1 && (evm_is_number(v))){
+        lv_obj_t * obj = (lv_obj_t*)evm_qml_object_get_pointer(p);
+        lv_slider_set_range(obj, evm_2_integer(v), lv_slider_get_max_value(obj));
+    }
+    return EVM_VAL_UNDEFINED;
+}
+
+static evm_val_t lvgl_qml_Slider_set_max(evm_t * e, evm_val_t *p, int argc, evm_val_t * v){
+    EVM_UNUSED(e);
+    if( argc >= 1 && (evm_is_number(v))){
+        lv_obj_t * obj = (lv_obj_t*)evm_qml_object_get_pointer(p);
+        lv_slider_set_range(obj, lv_slider_get_min_value(obj), evm_2_integer(v));
+    }
+    return EVM_VAL_UNDEFINED;
+}
+
+/********** Chart ***********/
+
+static evm_val_t  qml_Chart(evm_t * e, evm_val_t *p, int argc, evm_val_t * v){
+    EVM_UNUSED(e);
+    lv_obj_t * obj = NULL;
+    lv_obj_t * parent = lv_scr_act();
+    if(argc == 1) parent = evm_qml_object_get_pointer(v);
+    obj = (lv_obj_t*)lv_chart_create(parent, NULL);
+    if( !obj ) return EVM_VAL_UNDEFINED;
+    lvgl_qml_obj_add_style(obj);
+    evm_qml_object_set_pointer(p, obj);
+    return EVM_VAL_UNDEFINED;
+}
+
+static evm_val_t lvgl_qml_Chart_set_type(evm_t * e, evm_val_t *p, int argc, evm_val_t * v){
+    EVM_UNUSED(e);
+    if( argc >= 1 && evm_is_string(v)){
+        lv_obj_t * obj = (lv_obj_t*)evm_qml_object_get_pointer( p );
+        lv_chart_set_type(obj, lvgl_qml_get_ChartType(e, v));
+    }
+    return EVM_VAL_UNDEFINED;
+}
+
+static evm_val_t lvgl_qml_Chart_set_lineWidth(evm_t * e, evm_val_t *p, int argc, evm_val_t * v){
+    EVM_UNUSED(e);
+    if( argc >= 1 && evm_is_integer(v)){
+        lv_obj_t * obj = (lv_obj_t*)evm_qml_object_get_pointer( p );
+        lv_chart_set_series_width(obj, evm_2_integer(v));
+    }
+    return EVM_VAL_UNDEFINED;
+}
+
+static evm_val_t lvgl_qml_Chart_set_lineCounts(evm_t * e, evm_val_t *p, int argc, evm_val_t * v){
+    EVM_UNUSED(e);
+    if( argc >= 1 && evm_is_list(v)){
+        lv_obj_t * obj = (lv_obj_t*)evm_qml_object_get_pointer( p );
+        lv_chart_set_div_line_count(obj, evm_2_integer(evm_list_get(e, v, 0)), evm_2_integer(evm_list_get(e, v, 1)));
+    }
+    return EVM_VAL_UNDEFINED;
+}
+
+static evm_val_t lvgl_qml_Chart_set_yrange(evm_t * e, evm_val_t *p, int argc, evm_val_t * v){
+    EVM_UNUSED(e);
+    if( argc >= 1 && evm_is_list(v)){
+        lv_obj_t * obj = (lv_obj_t*)evm_qml_object_get_pointer( p );
+        lv_chart_set_range(obj, evm_2_integer(evm_list_get(e, v, 0)), evm_2_integer(evm_list_get(e, v, 1)));
+    }
+    return EVM_VAL_UNDEFINED;
+}
+
+/********** ChartSeries ***********/
+
+lv_chart_series_t * lv_chart_addseries(lv_obj_t * chart)
+{
+    lv_chart_ext_t * ext    = lv_obj_get_ext_attr(chart);
+    lv_chart_series_t * ser = lv_ll_ins_head(&ext->series_ll);
+    LV_ASSERT_MEM(ser);
+    if(ser == NULL) return NULL;
+
+    lv_coord_t def = LV_CHART_POINT_DEF;
+
+    if(ser == NULL) return NULL;
+
+    ser->points = lv_mem_alloc(sizeof(lv_coord_t) * ext->point_cnt);
+    LV_ASSERT_MEM(ser->points);
+    if(ser->points == NULL) {
+        lv_ll_rem(&ext->series_ll, ser);
+        lv_mem_free(ser);
+        return NULL;
+    }
+
+    ser->start_point = 0;
+
+    uint16_t i;
+    lv_coord_t * p_tmp = ser->points;
+    for(i = 0; i < ext->point_cnt; i++) {
+        *p_tmp = def;
+        p_tmp++;
+    }
+
+    ext->series.num++;
+
+    return ser;
+}
+
+static evm_val_t  qml_ChartSeries(evm_t * e, evm_val_t *p, int argc, evm_val_t * v){
+    EVM_UNUSED(e);
+    lv_chart_series_t * obj = NULL;
+    lv_obj_t * parent = lv_scr_act();
+    if(argc == 1) parent = evm_qml_object_get_pointer(v);
+
+    if (!strcmp(evm_qml_object_get_name(e, v), "Chart")){
+        obj = lv_chart_addseries(parent);
+    }
+    if( !obj ) return EVM_VAL_UNDEFINED;
+    evm_qml_object_set_pointer(p, obj);
+    return EVM_VAL_UNDEFINED;
+}
+
+static evm_val_t lvgl_qml_ChartSeries_set_data(evm_t * e, evm_val_t *p, int argc, evm_val_t * v){
+    if( argc >= 1 && evm_is_list(v)){
+        lv_obj_t * chart = (lv_obj_t *)evm_qml_object_get_pointer( evm_get_parent(e, *p) );
+        lv_chart_series_t * obj = (lv_chart_series_t*)evm_qml_object_get_pointer( p );
+        int len = evm_list_len(v);
+        lv_coord_t y_array[len];
+        for(int i=0; i< len; i++){
+            y_array[i] = evm_2_integer(evm_list_get(e, v, i));
+        }
+        lv_chart_set_points(chart, obj, y_array);
+    }
+    return EVM_VAL_UNDEFINED;
+}
+
+static evm_val_t lvgl_qml_ChartSeries_set_color(evm_t * e, evm_val_t *p, int argc, evm_val_t * v){
+    if( argc >= 1 && evm_is_string(v)){
+        lv_chart_series_t * obj = (lv_chart_series_t *)evm_qml_object_get_pointer( p );
+        obj->color =  lvgl_qml_style_get_color(e, v);
+    }
+    return EVM_VAL_UNDEFINED;
+}
+
 /********** border ***********/
 
 static evm_val_t qml_border_width(evm_t * e, evm_val_t *p, int argc, evm_val_t * v){
@@ -551,6 +771,36 @@ static evm_qml_value_reg_t qml_properties_Window[] = {
     {0, NULL, NULL}
 };
 
+static evm_qml_value_reg_t qml_properties_ProgressBar[] = {
+    {EVM_QML_INT, "minimumValue", (evm_native_fn)lvgl_qml_ProgressBar_set_min},
+    {EVM_QML_INT, "maximumValue", (evm_native_fn)lvgl_qml_ProgressBar_set_max},
+    {EVM_QML_INT, "value", (evm_native_fn)lvgl_qml_ProgressBar_set_value},
+    {0, NULL, NULL}
+};
+
+static evm_qml_value_reg_t qml_properties_Slider[] = {
+    {EVM_QML_INT, "minimumValue", (evm_native_fn)lvgl_qml_Slider_set_min},
+    {EVM_QML_INT, "maximumValue", (evm_native_fn)lvgl_qml_Slider_set_max},
+    {EVM_QML_INT, "value", (evm_native_fn)lvgl_qml_Slider_set_value},
+    {EVM_QML_CALLBACK, "onValueChanged", (evm_native_fn)NULL},
+    {0, NULL, NULL}
+};
+
+static evm_qml_value_reg_t qml_properties_Chart[] = {
+    {EVM_QML_STRING, "type", (evm_native_fn)lvgl_qml_Chart_set_type},
+    {EVM_QML_INT, "lineWidth", (evm_native_fn)lvgl_qml_Chart_set_lineWidth},
+    {EVM_QML_ANY, "lineCounts", (evm_native_fn)lvgl_qml_Chart_set_lineCounts},
+    {EVM_QML_ANY, "yRange", (evm_native_fn)lvgl_qml_Chart_set_yrange},
+    {0, NULL, NULL}
+};
+
+static evm_qml_value_reg_t qml_properties_ChartSeries[] = {
+    {EVM_QML_ANY, "data", (evm_native_fn)lvgl_qml_ChartSeries_set_data},
+    {EVM_QML_STRING, "color", (evm_native_fn)lvgl_qml_ChartSeries_set_color},
+    {0, NULL, NULL}
+};
+
+
 static evm_val_t qml_lv_init(evm_t * e, evm_val_t *p, int argc, evm_val_t * v){
     EVM_UNUSED(e);EVM_UNUSED(argc);EVM_UNUSED(v);
     int len = evm_prop_len(p);
@@ -580,6 +830,10 @@ int qml_lvgl_module(evm_t * e){
         {"CircularGauge", "Item", (evm_native_fn)qml_CircularGauge, qml_properties_CircularGauge},
         {"CircularGaugeStyle", NULL, (evm_native_fn)qml_CircularGaugeStyle, qml_properties_CircularGaugeStyle},
         {"Window", "Item", (evm_native_fn)qml_Window, qml_properties_Window},
+        {"ProgressBar", "Item", (evm_native_fn)qml_ProgressBar, qml_properties_ProgressBar},
+        {"Slider", "Item", (evm_native_fn)qml_Slider, qml_properties_Slider},
+        {"Chart", "Item", (evm_native_fn)qml_Chart, qml_properties_Chart},
+        {"ChartSeries", NULL, (evm_native_fn)qml_ChartSeries, qml_properties_ChartSeries},
         {NULL, NULL, NULL, NULL}
     };
 
