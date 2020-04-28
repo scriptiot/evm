6ae7e85b91efa7cd4707b1aa58311f080778a6d5
djf <ding465398889.com>
2020-04-28 21:22:50 +0800

    增加ejs构建

---
+++ ejs/src/main.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr.h>
+#include <sys/printk.h>
+#include <evm.h>
+
+void main(void)
+{
+	printk("Hello World! %s\n", CONFIG_BOARD);
+    evm_register_print((intptr_t)printk);
+}
