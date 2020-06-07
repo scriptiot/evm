9b8ada19aa744439479f9206e9043dfbdc4b2607
wzedmund <wzedmund@gmail.com>
2020-06-07 15:03:38 +0800

    add heatshrink

--- modules/heatshrink/compress_heatshrink.h
+++ modules/heatshrink/compress_heatshrink.h
@@ -1,17 +1,3 @@
-/*
- * This file is part of Espruino, a JavaScript interpreter for Microcontrollers
- *
- * Copyright (C) 2013 Gordon Williams <gw@pur3.co.uk>
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- *
- * ----------------------------------------------------------------------------
- *  Wrapper for heatshrink encode/decode
- * ----------------------------------------------------------------------------
- */
-
 typedef struct {
   unsigned char *ptr;
   size_t len;
